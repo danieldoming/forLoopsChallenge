3f22e1e9a7b3a93d2c5d4f9d74f972ee2a853162
daniel.dominguez <daniel.dominguez@MU-C1-02.mullen.pvt.k12.co.us>
2018-08-28 12:15:05 -0600

    for loops challenge

---
+++ forLoops/src/forLoops/forLoopsChallenge.java
@@ -0,0 +1,51 @@
+package forLoops;
+
+import java.util.Scanner;
+
+public class forLoopsChallenge
+	{
+		public static void main(String [] args) {
+			challengeOne();
+			challengeTwo();
+	}
+
+	
+		private static void challengeOne()
+			{
+		int totalSum = 0;
+		for(int i = 5; i<=100; i = i + 5) {
+			totalSum += i;
+			System.out.println(totalSum);
+		}
+	}
+		
+		// practice for Github
+		private static void challengeTwo(){
+			{
+				Scanner userInput = new Scanner(System.in);
+				
+				
+				System.out.println("Please choose a lower bound number.");
+				int lowerBound = userInput.nextInt();
+				
+				System.out.println("Please choose an upper bound number.");
+				int upperBound = userInput.nextInt();
+				
+				int totalSum = 0;
+				
+				for(; lowerBound <= upperBound; lowerBound++) {
+					totalSum += lowerBound;
+			
+				}
+				System.out.println(totalSum);
+				
+			}
+
+		}
+
+
+
+				
+	}
+
+	
